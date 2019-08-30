b67730f48371f2cca8d95f907bf3a5c1b128c7f0
Cerek Moreno <cerekmoreno@gmail.com>
2019-08-29 20:28:56 -0700

    almost done.

---
+++ game.js
@@ -0,0 +1,60 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var yourChoices = []
+
+// Creating variables to hold the number of wins, losses, and ties. They start at 0.
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+
+// Create variables that hold references to the places in the HTML where we want to display things.
+var directionsText = document.getElementById("directions-text");
+var userChoiceText = document.getElementById("userchoice-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesText = document.getElementById("guesses-text");
+
+// This function is run whenever the user presses a key.
+
+
+
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+
+
+
+document.onkeyup = function (event) {
+
+    // Determines which key was pressed.
+    var userGuess = event.key;
+    yourChoices.push(userGuess);
+
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
+    
+
+
+    // Reworked our code from last step to use "else if" instead of lots of if statements.
+
+    // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+    if (userGuess === computerGuess) {
+        wins++;
+
+    } else {
+        guessesLeft - 1;
+    }
+    if (guessesLeft = 0) {
+        losses++;
+    }
+
+   
+            // Hide the directions
+            directionsText.textContent = "";
+
+            // Display the user and computer guesses, and wins/losses/ties.
+            userChoiceText.textContent = "You chose: " + yourChoices;
+             winsText.textContent = "wins: " + wins;
+            lossesText.textContent = "losses: " + losses;
+            guessesText.textContent = "guesses left: " + guessesLeft;
+        }
+    
